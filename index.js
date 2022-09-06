c7bfb15232ba2b12aeaa0e0dfd8ccbc07c114da5
ASTkach <anatolii.s.tkach@gmail.com>
2022-09-06 16:59:45 +0300

    OOP-exercise

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
+class Inhabitants {
+    constructor(saying, species, name, gender, friends, legs) {
+        this.saying = saying + ", says the ";
+        this.species = species;
+        this.name = " by the name " + name + ".";
+        this.gender = " " + gender + ".";
+        this.friends = " Friends with " + friends + ".";
+        this.legs = " Has " + legs;
+    }
+    printInhabitants() {
+        return [this.saying + this.species + this.name + this.gender + this.friends + this.legs];
+    }
+}
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
+class Humans extends Inhabitants {
+    constructor(saying, name, gender, friends, legs, hands) {
+        super(saying, "human", name, gender, friends, legs);
+        this.hands = " " + hands;
+    }
+    printInhabitants() {
+        return [super.printInhabitants() + this.hands];
+    }
+}
 
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+class Dog extends Inhabitants {
+    constructor(saying, legs, name, gender, friends) {
+        super(saying, "dog", legs, name, gender, friends);
+    }
+}
 
+class Cat extends Inhabitants {
+    constructor(saying, legs, name, gender, friends) {
+        super(saying, "cat", legs, name, gender, friends);
+    }
+}
 
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
+let man = new Humans("'Honey, where's my wooden leg?'", "Franke", "Male", ["Elvis", " Marilyn"], "1 leg", "& 2 hands.");
+let woman = new Humans("'Well it's a good day for singing a song!'", "Marilyn", "Female", ["Franke", " Audrey"], "2 legs", "& 2 hands.");
+let animal1 = new Dog("'Wooffle-wooffle!'", "Elvis", "Male", ["Franke", " Marilyn", " Audrey"], "4 legs.");
+let animal2 = new Cat("'Hiss-hiss!'", "Audrey", "Female", "no one", "4 legs.");
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+const inhabitants = [man, woman, animal1, animal2];
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
-
+inhabitants.map((item) => print(`<h5>${item.printInhabitants()}</h5>`));
